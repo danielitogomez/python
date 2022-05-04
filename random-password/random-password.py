ceba567e4cbe0721d16989932493124a5f831085
danielitogomez <daniel.gomez.amaran@gmail.com>
2022-05-03 21:24:03 -0400

    feat: creating function

--- random-password/random-password.py
+++ random-password/random-password.py
@@ -13,22 +13,21 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# random password_list
-password_list = []
-for char in range(1, nr_letters + 1):
-  password_list.append(random.choice(letters))
+# Random password_list function
+def random_function():
+  password_list = []
+  for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
 
-for char in range(1, nr_symbols + 1):
-  password_list += random.choice(numbers)
+  for char in range(1, nr_symbols + 1):
+    password_list += random.choice(numbers)
 
-for char in range(1, nr_numbers + 1):
-  password_list += random.choice(symbols)
-#print(password_list)
-random.shuffle(password_list)
-#print(password_list)
+  for char in range(1, nr_numbers + 1):
+    password_list += random.choice(symbols)
+  random.shuffle(password_list)
 
-# print password
-password = ""
-for char in password_list:
-  password += char
-print(f"Your password is: {password}")
+  password = ""
+  for char in password_list:
+    password += char
+  print(f"Your password is: {password}")
+random_function()
